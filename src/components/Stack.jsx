f3b7538cd0ba4966e8377c53ed86bc3fa617d460
Daniel Reszka <dannn.r@gmail.com>
2021-03-08 21:38:02 +0100

    add component Stack

---
+++ src/components/Stack.jsx
@@ -0,0 +1,25 @@
+export function Stack({ children, className = "", spacing = "md" }) {
+  return (
+    <>
+      <div className={`stack-${spacing} ${className}`}>{children}</div>
+
+      <style jsx>{`
+        .stack-xs :global(> * + *) {
+          margin-top: 0.5rem;
+        }
+
+        .stack-sm :global(> * + *) {
+          margin-top: 1rem;
+        }
+
+        .stack-md :global(> * + *) {
+          margin-top: 1.5rem;
+        }
+
+        .stack-lg :global(> * + *) {
+          margin-top: 2rem;
+        }
+      `}</style>
+    </>
+  );
+}
