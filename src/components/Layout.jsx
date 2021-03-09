39b7960ac59a3f4ed9ca5f848c8ffa89334f651f
Daniel Reszka <dannn.r@gmail.com>
2021-03-09 02:43:12 +0100

    fix fixed bg not working on ios

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,8 @@
 import { Stack } from "./Stack";
-import { Stars } from "./Stars";
 
 export function Layout({ children }) {
   return (
     <>
-      <Stars />
-
       <div className="container mx-auto max-w-3xl text-center">
         <Stack spacing="lg">
           {children}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,18 @@
+import { BgFixed } from "../components/BgFixed";
+import { Stars } from "../components/Stars";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <BgFixed />
+      <Stars />
+
+      <div className="relative z-10">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
 
 export default MyApp;
