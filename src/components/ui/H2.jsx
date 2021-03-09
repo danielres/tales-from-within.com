d4b3d061a15b76bd80bb9aa805c4a69b74c0ec64
Daniel Reszka <dannn.r@gmail.com>
2021-03-09 19:55:41 +0100

    H2: make white + add support for alignment

--- src/components/ui/H2.jsx
+++ src/components/ui/H2.jsx
@@ -1,7 +1,13 @@
-export function H2({ children }) {
+export function H2({ children, align = "center" }) {
   return (
     <h2
-      className="text-xl"
+      className={`text-xl text-white text-opacity-75 ${
+        align === "left"
+          ? "text-left"
+          : align === "right"
+          ? "text-right"
+          : "text-center"
+      } px-4`}
       style={{
         textShadow:
           "rgb(193, 238, 255) 0px 0px 8px, rgb(193, 238, 255) 0px 0px 16px, rgb(240, 238, 255) 0px 0px 24px ",
