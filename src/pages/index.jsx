0622d5496e4344ae6e0a044bb7f4c7b0f6460d69
Daniel Reszka <dannn.r@gmail.com>
2021-03-09 17:45:54 +0100

    SCTracks: improve + iterate

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import { Layout } from "../components/Layout";
 import { Stack } from "../components/Stack";
 
+const tracks = ["reefs", "dreamstate"];
+
 export default function Home() {
   return (
     <>
@@ -31,12 +33,11 @@ export default function Home() {
         <main>
           <Stack spacing="xl">
             <Stack spacing="xs">
-              <Card>
-                <SCTrack id={912679432} />
-              </Card>
-              <Card>
-                <SCTrack id={869495029} />
-              </Card>
+              {tracks.map((slug) => (
+                <Card>
+                  <SCTrack track={slug} />
+                </Card>
+              ))}
             </Stack>
 
             <Card>
@@ -99,15 +100,26 @@ function SCPlaylist() {
   );
 }
 
-function SCTrack({ id }) {
+function SCTrack({
+  id,
+  track = "reefs",
+  artist = "tales-from-within",
+  color = "5d5c62",
+  inverse = true,
+  autoPlay = false,
+  showUser = false,
+  size = 1,
+}) {
+  const url = `https://soundcloud.com/${artist}/${track}`;
+
   return (
     <iframe
       width="100%"
-      height="20"
+      height={size === 1 ? 20 : size === 2 ? 130 : 20}
       scrolling="no"
       frameborder="no"
       allow="autoplay"
-      src={`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${id}&color=%235d5c62&inverse=true&auto_play=false&show_user=false`}
+      src={`https://w.soundcloud.com/player/?url=${url}&color=%23${color}&inverse=${inverse}&auto_play=${autoPlay}&show_user=${showUser}`}
     ></iframe>
   );
 }
