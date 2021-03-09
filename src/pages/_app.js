72b98b74069893247813200b233ce9f2e67beb1f
Daniel Reszka <dannn.r@gmail.com>
2021-03-09 04:26:44 +0100

    add rotating spiral effect

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,18 @@
 import { BgFixed } from "../components/BgFixed";
+// import { Flicker } from "../components/Flicker";
+import { Spiral } from "../components/Spiral";
 import { Stars } from "../components/Stars";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <BgFixed />
-      <Stars />
+      <div className="relative z-0">
+        <BgFixed />
+        <Spiral />
+        <Stars />
+        {/* <Flicker /> */}
+      </div>
 
       <div className="relative z-10">
         <Component {...pageProps} />
