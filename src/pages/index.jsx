fe955e184055b81b4c9e7bec1924d7c8485f4586
Daniel Reszka <dannn.r@gmail.com>
2021-03-09 04:27:38 +0100

    SCPlaylist: remove clutter from widget

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -85,7 +85,10 @@ function SCPlaylist() {
       scrolling="no"
       frameborder="no"
       allow="autoplay"
-      src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/504227907&color=%233bb4cb&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+      src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/504227907&color=%233bb4cb&auto_play=false&hide_related=true&show_comments=true&show_user=false&show_reposts=false&show_teaser=false&show_artwork=false&sharing=false&show_playcount=false"
+    ></iframe>
+  );
+}
     ></iframe>
   );
 }
