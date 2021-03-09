e2e7ad725e831b5087897ff66531796b1f80e846
Daniel Reszka <dannn.r@gmail.com>
2021-03-09 16:43:27 +0100

    Spiral: fix dimensions + positioning

--- src/components/Spiral.js
+++ src/components/Spiral.js
@@ -2,18 +2,36 @@ export function Spiral() {
   return (
     <div className="relative z-50">
       <div className="rotate"></div>
+      {/* <div className="rotate2"></div> */}
       <style jsx>
         {`
           .rotate {
             position: fixed;
             z-index: 0;
-            width: 2000px;
-            height: 2000px;
-            top: -800px;
-            left: 00px;
+            width: 150vw;
+            height: 150vw;
+            left: -25vw;
+            top: -75vw;
+            animation: spin 200s linear infinite;
+            background-image: url("/spiral.png");
+            background-repeat: no-repeat;
+
+            background-size: contain;
+            background-position: center;
+          }
+          .rotate2 {
+            transform: rotate(90deg);
+            position: fixed;
+            z-index: 0;
+            width: 100vw;
+            height: 100vw;
+            left: 0vw;
+            top: -40vw;
             animation: spin 150s linear infinite;
             background-image: url("/spiral.png");
             background-repeat: no-repeat;
+            animation-fill-mode: backwards;
+
             background-size: contain;
             background-position: center;
           }
